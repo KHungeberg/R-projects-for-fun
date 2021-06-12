26b970205f41233725548a03a69245c1d4be1463
KHungeberg <85743680+KHungeberg@users.noreply.github.com>
2021-06-12 12:41:50 +0200

    Update Algoritme til at finde maksimale afstand.R

--- Algoritme til at finde maksimale afstand.R
+++ Algoritme til at finde maksimale afstand.R
@@ -1,19 +1,20 @@
-#Algoritmisk metode til at finde maksimal længde: 
+# Algorithm to find the maximal distance between two spheres in space. 
 
-#Vi vil gerne have tilfældige retninger på vektoren i kuglerne. 
-#Dette vil vi gøre ved at bestemme theta og phi uniform ift hhv (0;pi) og [0;2pi]
+#My method is creating a lot of random vectors within the parameterisation of the two spheres. The spheres are independently at an arbitrary point in R^3. 
+# To create these random vectors within the sphere i use random spherical coordinates, letting theta be in (0;pi) og phi in [0;2pi]. 
+# The "randomness" of these values will come from samples of a uniform distribution. 
 
-#Hvis en kugle i rummet (R^3) så husker vi at kuglens ligning er (x-a)^2+(y-b)^2+(z-c)^2=r^2 
-#Hvor centrum af kuglen er i koordinatet (a,b,c). Parameterfremstillingen ville således være: 
-#c(a,b,c)+r*c(sin(theta)*cos(phi),sin(phi)*sin(theta),cos(theta))
+# If a sphere is in space (R^3) we remember their equation: (x-a)^2+(y-b)^2+(z-c)^2=r^2
+# Where the center of the sphere is the point (a,b,c). A known parametisation of the spere is then: 
+# c(a,b,c)+r*c(sin(theta)*cos(phi),sin(phi)*sin(theta),cos(theta))
 
-Kugle_1 <- matrix(NA,n,3)#Tom vektor hvor hver række er koordinaterne til et tilfældigt punkt på periferien af kugle1
+Kugle_1 <- matrix(NA,n,3)# Empty matrix where every entrance is a random point on the periphery on sphere 1
 
-Kugle_2 <- matrix(NA,n,3)#Samme som der står ovenfor, bare for kugle 2. 
+Kugle_2 <- matrix(NA,n,3)# Empty matrix where every entrance is a random point on the periphery on sphere 2
 
-n <- 100000 #Antallet af punkter vi kigger på. Teoretisk set burde vi nå den sande maksimale afstande for n -> uendelig
+n <- 100000 # Number of points 
 
-#Her laver jeg et loop som skaber de tilfældige punkter på kuglerne 
+# Here i create a loop creating the random points. 
 for(i in 1:n){
 
 thetasample <- runif(1,min = 0,max = pi)
@@ -22,22 +23,21 @@ phisammple <- runif(1,min= 0,max=2*pi)
 thetasample_2 <- runif(1,min = 0,max = pi)
 phisammple_2 <- runif(1,min= 0,max=2*pi)
 
-Kugle_1[i,] <- c(3,-2,2)-3*c(sin(thetasample)*cos(phisammple),sin(phisammple)*sin(thetasample),cos(thetasample))
-#Kugle 1 har  -3*(sinus/cosinus vektor) fordi det giver den omvendte retning af kugle 2 cosinus/sinus vektor. 
+Kugle_1[i,] <- c(3,-2,2)+3*c(sin(thetasample)*cos(phisammple),sin(phisammple)*sin(thetasample),cos(thetasample))
 
 Kugle_2[i,] <- c(13,8,12)+2*c(sin(thetasample_2)*cos(phisammple_2),sin(phisammple_2)*sin(thetasample_2),cos(thetasample_2))
 
 }
-Kuglediff <- Kugle_1-Kugle_2 #Tager differensen af alle de tilfældige punkter
+Kuglediff <- Kugle_1-Kugle_2 # Measures the differense of all the points. 
 
-afs <- rep(NA,n)#Tom vektor til længden mellem punkterne i kuglerne
+afs <- rep(NA,n)# Empty vector to calculate the lenght of all the vectors in kuglediff
 
-#Loop som udregner normen af vektoren der opstår ved differensen af hvert punkt. 
+# Loop calculating the norm of every vector in kuglediff
 for(i in 1:n){
   afs[i] <- sqrt(sum((Kuglediff[i,])^2))
 }
-max(afs) #Tager den maksimale afstand vi får ved at tage tilfældige punkter
+max(afs) # Taking the biggest distance in afs
 
-sqrt(3)*10+5-max(afs) #Angiver hvor stor vores error 
+sqrt(3)*10+5-max(afs) #Calculating difference between the theoretically correct result and out approximation. 
 
 
